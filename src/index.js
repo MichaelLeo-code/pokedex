e46e7b5cc54e344320c103139e72c345118fbd66
MichaelLeo-code <michael.leo.dk@gmail.com>
2024-04-02 09:20:24 +0200

    idk

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { Link, RouterProvider, createHashRouter } from "react-router-dom"
 import App from "./App"
+
+const router = createHashRouter([
+    {
+        path: "/",
+        element: 
+        <>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+            </nav>
+        </>
+    },
+    {
+        path: "/about",
+        element: <h1>About</h1>
+    }
+])
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <RouterProvider router={router}/>
+    // <React.StrictMode>
+    //     <App />
+    // </React.StrictMode>
 )
